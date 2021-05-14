98328bff6dfd3dd6818b2c3ecf25c4b49da88835
profe <profe@192.168.100.12>
2021-05-13 21:35:08 -0300

    REQ02ConsultarLivro - validacao do comportamento

--- scel/src/main/java/com/fatec/scel/model/LivroRepository.java
+++ scel/src/main/java/com/fatec/scel/model/LivroRepository.java
@@ -1,9 +1,15 @@
 package com.fatec.scel.model;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LivroRepository extends CrudRepository <Livro, Long>{
+	
+	public Livro findByIsbn(@Param("isbn") String isbn);
+	List<Livro> findAllByTituloIgnoreCaseContaining(String titulo);
 	
 }
