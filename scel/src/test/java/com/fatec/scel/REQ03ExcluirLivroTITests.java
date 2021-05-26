ece6d31cd943b464e0fdfef57edcbeceb0032e64
profe <profe@192.168.100.17>
2021-05-26 19:19:06 -0300

    Implementação do page object

---
+++ scel/src/test/java/com/fatec/scel/REQ03ExcluirLivroTITests.java
@@ -0,0 +1,30 @@
+package com.fatec.scel;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fatec.scel.model.Livro;
+import com.fatec.scel.model.LivroRepository;
+@SpringBootTest
+class REQ03ExcluirLivroTITests {
+	
+	@Autowired
+	LivroRepository repository;
+
+	@Test
+	void ct01_quando_exclui_livro_pelo_id_consulta_por_isbn_deve_retornar_null() {
+		//Dado que o livro esta cadastrado
+		repository.deleteAll();
+		Livro livro = new Livro("3333", "Teste de Software","Delamaro");
+		repository.save(livro);
+		//Quando - o usuário solicita a exclusao
+		Livro ro = repository.findByIsbn("3333");
+		repository.deleteById(ro.getId());
+		//Então - o resultado obtido da consulta deve ser null
+		assertThat(repository.findByIsbn("3333")).isEqualTo(null);
+	}
+
+}
