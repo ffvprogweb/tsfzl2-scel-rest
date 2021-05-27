f4e95dabad22167035258e6692402418007a6d61
profe <profe@192.168.100.17>
2021-05-27 08:11:07 -0300

    Inserir fixture – cadastra um registro no db na inicialização.

--- scel/src/main/java/com/fatec/scel/ScelApplication.java
+++ scel/src/main/java/com/fatec/scel/ScelApplication.java
@@ -3,6 +3,9 @@ package com.fatec.scel;
 import java.util.List;
 import java.util.stream.LongStream;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +16,18 @@ import com.fatec.scel.model.LivroRepository;
 
 @SpringBootApplication
 public class ScelApplication {
-
+	Logger logger = LogManager.getLogger(ScelApplication.class);
+	@Autowired
+	LivroRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(ScelApplication.class, args);
 	}
-	
+	@Autowired
+	public void inicializa() {
+		Livro livro = new Livro("1112", "Engenharia de Software", "Pressman");
+		repository.save(livro);
+		Livro umLivro = repository.findByIsbn("1112");
+		logger.info(">>>>>> inicializacao da aplicacao =>  " + umLivro.toString());
+		
+	}
 }
